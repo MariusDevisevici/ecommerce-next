4ae4b1e69786574529203662f553e3685fe3f83f
unknown <mariusdevisevici.ro>
2022-10-23 18:57:28 +0300

    top products slider

--- components/ProductsSlide.tsx
+++ components/ProductsSlide.tsx
@@ -13,12 +13,14 @@ const TopProductsSlide = ({
   linkTo: string;
 }) => {
   return (
-    <div className="w-72">
-      <Image
-        src={`https://res.cloudinary.com/dxynua9bn/image/upload/v1666519649/products/${image}`}
-        width={"300px"}
-        height={"200px"}
-      />
+    <div className="w-72 select-none">
+      <div className="w-80 h-52 bg-cover relative pointer-events-auto z-10">
+        <Image
+          className="pointer-events-none"
+          src={`https://res.cloudinary.com/dxynua9bn/image/upload/v1666519649/products/${image}`}
+          layout={"fill"}
+        />
+      </div>
       <h2 className="uppercase text-gray-600 mt-3 text-xs tracking-widest">
         {category}
       </h2>
--- components/TopProducts.tsx
+++ components/TopProducts.tsx
@@ -5,17 +5,17 @@ import ProductsSlide from "./ProductsSlide";
 
 const responsive = {
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 3000, min: 995 },
     items: 4,
     partialVisibilityGutter: 40, // this is needed to tell the amount of px that should be visible.
   },
   tablet: {
-    breakpoint: { max: 1024, min: 594 },
+    breakpoint: { max: 995, min: 755 },
     items: 3,
     partialVisibilityGutter: 30, // this is needed to tell the amount of px that should be visible.
   },
   mobile: {
-    breakpoint: { max: 594, min: 0 },
+    breakpoint: { max: 755, min: 0 },
     items: 2,
     partialVisibilityGutter: 30, // this is needed to tell the amount of px that should be visible.
   },
